bf24ea586f95bce83f30cae6968cc47bea319051
Sambhav Rijal <sambhavrijal16@gmail.com>
2023-06-29 21:37:13 +0545

    main: set title

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Veggie</title>
+      </head>
         <body className={inter.className}>
           {children}</body>
     </html>
